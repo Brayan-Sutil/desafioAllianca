1e197085ce842215bc006f9eca4919d399c7f4c8
BrayanSutil <brayansutil81@gmail.com>
2023-10-14 14:52:27 -0300

    feat: TodoItem final implementation

---
+++ src/components/TodoItem/TodoItemStyled.ts
@@ -0,0 +1,17 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import { List } from "@mui/material";
+
+import styled from "styled-components";
+
+const IconDelete = styled(DeleteIcon)`
+    margin-right: 10px;
+    color: red;
+`;
+
+const BoxList = styled(List)`
+    width: 100%;
+    margin-top: 25px;
+`;
+
+
+export { IconDelete, BoxList };
