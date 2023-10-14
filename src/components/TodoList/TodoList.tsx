02f02d252d08ed300e6b044a38b4b6c3b25be89b
BrayanSutil <brayansutil81@gmail.com>
2023-10-14 14:55:09 -0300

    feat: TodoList final implementation

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import TodoItem from "../TodoItem/TodoItem";
 import TodoForm from "../TodoForm/TodoForm";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+import { useTodos } from "../../Context/TodoContext";
 
 const TodoList = () => {
+  const { todos } = useTodos();
   return (
-    <Container maxWidth="sm">
+    <Container
+      maxWidth="sm"
+      style={{
+        backgroundColor: "white",
+        padding: "15px",
+        borderRadius: "10px",
+      }}
+    >
+      <Typography fontSize={25} fontWeight={600} >Todo List</Typography>
       <TodoForm />
-      <TodoItem />
+      {todos.length < 1 ? (
+        <Typography fontSize={20} marginTop={2}>You don't have a task</Typography>
+      ) : (
+        todos.map((todo) => <TodoItem key={todo.id} {...todo} />)
+      )}
     </Container>
   );
 };
